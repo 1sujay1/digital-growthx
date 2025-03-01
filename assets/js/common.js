3f11b6a4b32343f65679e50f24172e4c0cc01bb2
logesh-aimwindow <Logeshwaran.p@aimwindow.co.in>
2025-03-01 20:52:45 +0530

    feat: files updated

---
+++ assets/js/common.js
@@ -0,0 +1,229 @@
+const headerHTML = `<header class="header-area">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container-fluid">
+      <!-- Logo -->
+      <a class="navbar-brand" href="/">
+        <img src="/assets/img/logo/black-logo.png" alt="Digital GrowthX Logo" />
+      </a>
+
+      <!-- Mobile Toggle -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#mainNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Navigation Menu -->
+      <div class="collapse navbar-collapse" id="mainNav">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active fw-bold" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold" href="/service">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold" href="/contact">Contact</a>
+          </li>
+        </ul>
+
+        <!-- Get Started Button -->
+        <div class="d-flex ms-lg-4">
+          <a href="/contact" class="btn btn-primary"> Get in Touch </a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</header>
+`;
+
+const footerHTML = `
+ <section class="footer-section footer-bg fix">
+            <div class="container">
+                <div class="footer-widgets-wrapper">
+                    <div class="row">
+                        <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".2s">
+                            <div class="single-footer-widget">
+                                <div class="widget-head">
+                                    <a href="/">
+                                        <img src="/assets/img/logo/white-logo.png" alt="img">
+                                    </a>
+                                </div>
+                                <div class="footer-content">
+                                    <p>
+                                        Phasellus ultricies aliquam volutpat 
+                                        ullamcorper laoreet neque, a lacinia
+                                        curabitur lacinia mollis
+                                    </p>
+                                    <div class="social-icon d-flex align-items-center">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                        <a href="#"><i class="fab fa-youtube"></i></a>
+                                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 ps-lg-5 wow fadeInUp" data-wow-delay=".4s">
+                            <div class="single-footer-widget">
+                                <div class="widget-head">
+                                    <h3>Quick Links</h3>
+                                </div>
+                                <ul class="list-area">
+                                    <li>
+                                        <a href="/about">
+                                            <i class="fa-solid fa-chevrons-right"></i>
+                                            About Us
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="/service">
+                                            <i class="fa-solid fa-chevrons-right"></i>
+                                            Our Services
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="/news">
+                                            <i class="fa-solid fa-chevrons-right"></i>
+                                            Our Blogs
+                                        </a>
+                                    </li>
+                                    <!-- <li>
+                                        <a href="/faq">
+                                            <i class="fa-solid fa-chevrons-right"></i>
+                                            FAQ’S
+                                        </a>
+                                    </li> -->
+                                    <li>
+                                        <a href="/contact">
+                                            <i class="fa-solid fa-chevrons-right"></i>
+                                            Contact Us
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".6s">
+                            <div class="single-footer-widget">
+                                <div class="widget-head">
+                                    <h3>Recent Posts</h3>
+                                </div>
+                                <div class="recent-post-area">
+                                    <div class="recent-post-items">
+                                        <div class="thumb">
+                                            <img src="/assets/img/news/pp1.jpg" alt="post-img">
+                                        </div>
+                                        <div class="content">
+                                            <ul class="post-date">
+                                                <li>
+                                                    <i class="fa-solid fa-calendar-days me-2"></i>
+                                                    20 Feb, 2024
+                                                </li>
+                                            </ul>
+                                            <h6>
+                                                <a href="/news-detail">
+                                                    Top 5 Most Famous <br>
+                                                    Technology Trend In 2024
+                                                </a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div class="recent-post-items mb-0">
+                                        <div class="thumb">
+                                            <img src="/assets/img/news/pp2.jpg" alt="post-img">
+                                        </div>
+                                        <div class="content">
+                                            <ul class="post-date">
+                                                <li>
+                                                    <i class="fa-solid fa-calendar-days me-2"></i>
+                                                    15 Dec, 2024
+                                                </li>
+                                            </ul>
+                                            <h6>
+                                                <a href="/news-details">
+                                                    The Surfing Man Will Blow <br>
+                                                    Your Mind   
+                                                </a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 ps-xl-5 wow fadeInUp" data-wow-delay=".8s">
+                            <div class="single-footer-widget">
+                                <div class="widget-head">
+                                    <h3>Contact Us</h3>
+                                </div>
+                                <div class="footer-content">
+                                    <ul class="contact-info">
+                                        <li>
+                                            <i class="fa-regular fa-envelope"></i>
+                                            <a href="mailto:info@example.com">digitalgrowthx@gmail.com</a>
+                                        </li>
+                                        <li>
+                                            <i class="fa-solid fa-phone-volume"></i>
+                                            <a href="tel:2086660112">+91-9952402011</a>
+                                        </li>
+                                    </ul>
+                                    <div class="footer-input">
+                                        <input type="email" id="email2" placeholder="Your email address">
+                                        <button class="newsletter-btn" type="submit">
+                                            <i class="fa-regular fa-arrow-right-long"></i>
+                                        </button>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked="">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            I agree to the <a href="/contact">Privacy Policy.</a>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="footer-wrapper d-flex align-items-center justify-content-between">
+                        <p class="wow fadeInLeft color-2" data-wow-delay=".3s">
+                            © All Copyright 2025 by Digital Growthx
+                        </p>
+                        <ul class="footer-menu wow fadeInRight" data-wow-delay=".5s">
+                           <!--  <li>
+                                <a href="/contact">
+                                    Terms & Condition       
+                                </a>
+                            </li> -->
+                            <li>
+                                <a href="/contact">
+                                    Contact Us
+                                </a>
+                            </li>
+                        </ul> 
+                    </div>
+                </div>
+                <a href="#" id="scrollUp" class="scroll-icon">
+                    <i class="far fa-arrow-up"></i>
+                </a>
+            </div>
+        </section>
+`;
+const backToTopHTML = `
+<button id="back-top" class="back-to-top">
+      <i class="fa-regular fa-arrow-up"></i>
+    </button>
+`;
+const mouseCurzorDesignHTML = `    <div class="mouse-cursor cursor-outer"></div>
+    <div class="mouse-cursor cursor-inner"></div>`;
+document.querySelector("#header").innerHTML = headerHTML;
+document.querySelector("#footer").innerHTML = footerHTML;
+document.querySelector("body").innerHTML += backToTopHTML;
+document.querySelector("body").innerHTML += mouseCurzorDesignHTML;
